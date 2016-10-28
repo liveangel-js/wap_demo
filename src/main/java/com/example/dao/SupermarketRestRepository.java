afeadcf4cc3ccfcb770a07014c9641dbe91ac5de
liveangel <jiangshu.social@gmail.com>
2016-10-28 22:24:50 +0800

    超市REST; 属性查询.

---
+++ src/main/java/com/example/dao/SupermarketRestRepository.java
@@ -0,0 +1,17 @@
+package com.example.dao;
+
+import com.example.model.Supermarket;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Created by liveangel on 2016-10-28.
+ */
+@RepositoryRestResource(collectionResourceRel = "supermarket", path = "supermarkets")
+public interface SupermarketRestRepository extends PagingAndSortingRepository<Supermarket, Long> {
+    List<Supermarket> findByAddressContaining(@Param("address") String address);
+    List<Supermarket> findByTelContaining(@Param("tel") String tel);
+}
