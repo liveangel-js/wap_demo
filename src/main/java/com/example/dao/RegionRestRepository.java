f623d2c4dc0f869a1efa3a53e1f8b1e79f5fd72f
liveangel <jiangshu.social@gmail.com>
2016-10-28 23:17:48 +0800

    地区REST; 根据经理ID查地区

---
+++ src/main/java/com/example/dao/RegionRestRepository.java
@@ -0,0 +1,22 @@
+package com.example.dao;
+
+import com.example.model.Region;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Created by liveangel on 2016-10-28.
+ */
+public interface RegionRestRepository extends PagingAndSortingRepository<Region, Long> {
+    List<Region> findByNameContaining(@Param("name") String name);
+    List<Region> findByManagerIn(@Param("managerId") String managerId);
+
+//    @Query("select r from Region r where r.manager = ?1")
+//    List<Region> findByManagerId(@Param("managerId") String managerId);
+    @Query(value = "SELECT * FROM Region WHERE manager_id = ?1", nativeQuery = true)
+    List<Region> findByManagerId(@Param("managerId") String managerId);
+}
