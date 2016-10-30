b724fa8d1a6d5571736b63801b2b23c15dffd0c4
liveangel <jiangshu.social@gmail.com>
2016-10-30 10:32:15 +0800

    customer report; sale report;

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -12,7 +12,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("base/index");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/inherit").setViewName("inherit");
-        registry.addViewController("/sale/report").setViewName("sale/report");
+        registry.addViewController("/sale_report").setViewName("sale/report");
+        registry.addViewController("/report_cus").setViewName("sale/report_cus");
+        registry.addViewController("/report_unsalable").setViewName("sale/report_unsalable");
 //        registry.addViewController("/footer").setViewName("footer");
 //        registry.addViewController("/login").setViewName("login");
     }
